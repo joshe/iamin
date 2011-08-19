1514ec145a7f6039bd98a57d3308915cad2b93ee
Josh Evnin <jevnin@gmail.com>
2011-08-18 19:20:38 -0500

    Fixing migration on heroku

--- db/migrate/20110816031629_kill_join_events_table_and_add_events_users_table.rb
+++ db/migrate/20110816031629_kill_join_events_table_and_add_events_users_table.rb
@@ -1,6 +1,5 @@
 class KillJoinEventsTableAndAddEventsUsersTable < ActiveRecord::Migration
   def self.up
-    drop_table :join_events
     create_table :events_users, :id => false do |t|
         t.integer "event_id"
         t.integer "user_id"
@@ -9,13 +8,6 @@ class KillJoinEventsTableAndAddEventsUsersTable < ActiveRecord::Migration
   end
 
   def self.down
-    remove_index :events_users, ["event_id", "user_id"]
     drop_table :events_users
-    create_table :join_events do |t|
-      t.references :user
-      t.references :event
-      
-      t.timestamps
-    end
   end
 end
